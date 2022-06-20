dcc29864ff36d6f9119f9395a9153fd360387706
coach paco <paco@csedge.org>
2022-06-20 12:48:36 -0700

    added lottie animations

--- View Controllers/ViewController.swift
+++ View Controllers/ViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
+    
+    var animationView : AnimationView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        //TESTING LOTTIE
+        animationView = .init(name: "thunder")
+        animationView?.loopMode = .loop
+        animationView?.frame = self.view.bounds
+        view.addSubview(animationView!)
+        animationView?.animationSpeed = 0.8
+        animationView?.play()
+        
+        
     }
 
 
